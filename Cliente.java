8bb30f077c2e6312b0e4488d1aa85bcdc81bc63b
Mayara Barbosa <maay.bs@hotmail.com>
2018-09-16 15:50:05 -0300

    Add files via upload
    ok

--- Cliente.java
+++ Cliente.java
@@ -2,6 +2,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.util.Calendar;
 import java.util.Random;
 
 public class Cliente {
@@ -9,22 +10,24 @@ public class Cliente {
     public static void main(String[] args) throws Exception {
 
         //join no grupo
+        Calendar c = Calendar.getInstance();
 
         int porta = 8080;
         MulticastSocket mSocket = new MulticastSocket(porta);
         InetAddress endereco = InetAddress.getByName("224.0.0.1");
         mSocket.joinGroup(endereco);
-        System.out.println("usuario conectado");
-        long horarioCord;
+        System.out.println("Usuario conectado");
+        long horarioCord, diff, horarioAtualizado;
 
         DatagramSocket uniSocket = new DatagramSocket();
         Random aleatorio = new Random();
         long clientTime = (System.currentTimeMillis()-aleatorio.nextInt(1000000));
-
+        c.setTimeInMillis(clientTime);
+        System.out.println("Horário inicial do cliente: "+ c.getTime());
 
         //e fica esperando alguma mensagem do coordenador
         while(true) {
-            DatagramPacket pctRecebido, pctEnviado;
+            DatagramPacket pctRecebido, pctEnviado, pctFinal;
             pctRecebido = new DatagramPacket(new byte[1024],1024);
             mSocket.receive(pctRecebido);
             String resposta = new String(pctRecebido.getData(),pctRecebido.getOffset(),pctRecebido.getLength());
@@ -38,9 +41,19 @@ public class Cliente {
             pctEnviado = new DatagramPacket(msg.getBytes(), msg.length(), pctRecebido.getAddress(),pctRecebido.getPort());
             uniSocket.send(pctEnviado);
 
+            //recebe diferença para correcao
+            pctFinal = new DatagramPacket(new byte[1024], 1024);
+            uniSocket.receive(pctFinal);
+            diff = Long.parseLong(new String(pctFinal.getData(),pctFinal.getOffset(),pctFinal.getLength()));
+            System.out.println("diferenca: " + diff);
+            horarioAtualizado = clientTime - (-1*diff);
+            c.setTimeInMillis(horarioAtualizado);
+            System.out.println("Horário atualizado do cliente: " + c.getTime());
 
+            break;
 
         }
+
         //long horario = System.currentTimeMillis();
         //Calendar c = Calendar.getInstance();
         //c.setTimeInMilis(horario);
--- Coordenador.java
+++ Coordenador.java
@@ -2,19 +2,25 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Coordenador {
 
     public static void main(String[] args) throws Exception {
+
+        Calendar c = Calendar.getInstance();
 
         DatagramSocket uSocket = new DatagramSocket();
         int porta = 8080;
         InetAddress endereco = InetAddress.getByName("224.0.0.1");
         HashMap<Integer, Long> clientes = new HashMap<>();
         long coordenadorTime = System.currentTimeMillis();
-        long correcao, sum = 0;
+        long correcao, sum = 0, corrigido;
 
+        c.setTimeInMillis(coordenadorTime);
+        System.out.println("Horario inicial coordenador: " +c.getTime());
         String msg = Long.toString(coordenadorTime);
         DatagramPacket pct = new DatagramPacket(msg.getBytes(),msg.length(),endereco, porta);
         uSocket.send(pct);
@@ -32,16 +38,30 @@ public class Coordenador {
                 //uSocket.setSoTimeout(5000 + uSocket.getSoTimeout());
             }
         }catch(Exception e){
-            System.out.println("Nao tem ngm conectado");
+            System.out.println("Nao tem mais ngm conectado");
         }
 
         for (HashMap.Entry<Integer,Long> pair : clientes.entrySet()) {
-            System.out.println(pair.getKey());
-            System.out.println(pair.getValue());
+//            System.out.println("porta: " +pair.getKey());
+//            System.out.println("valor: " + pair.getValue());
              sum =+ pair.getValue();
         }
-        correcao = sum/4;
+//        System.out.println(sum);
+        correcao = sum/4; //rodar 3 cli
+//        System.out.println("correcao " + correcao);
+        corrigido = coordenadorTime - (-1* correcao);
 
+        for (HashMap.Entry<Integer,Long> pair : clientes.entrySet()) {
+            pair.setValue(pair.getValue()-(correcao * -1));
+//            System.out.println("valor enviado: "+ pair.getValue());
+            String aux = Long.toString(pair.getValue());
+            DatagramPacket diff = new DatagramPacket(aux.getBytes(),aux.length(),endereco, pair.getKey());
+            uSocket.send(diff);
+        }
+
+        c.setTimeInMillis(corrigido);
+
+        System.out.println("Novo horario do coordenador é: "+ c.getTime());
 
 
     }
